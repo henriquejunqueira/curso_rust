df14d1a010c09cba5f78f34eee22236febab7cdc
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-02-13 08:49:14 -0300

    Trabalhando com a estrutura enum utilizando valores associados

---
+++ modulo05/aula02/src/main.rs
@@ -0,0 +1,19 @@
+enum Coordenada {
+    DoisD(i32, i32),
+    TresD(i32, i32, i32),
+}
+
+fn main() {
+    let ponto2d = Coordenada::DoisD(5, 10);
+    let ponto3d = Coordenada::TresD(3, 8, 15);
+
+    match ponto2d {
+        Coordenada::DoisD(x, y) => println!("Coordenada 2d: {}, {}", x, y),
+        Coordenada::TresD(_, _, _) => println!("Coordenada 3d"),
+    }
+
+    match ponto3d {
+        Coordenada::DoisD(_, _) => println!("Coordenada 2d"),
+        Coordenada::TresD(x, y, z) => println!("Coordenada 3d: {}, {}, {}", x, y, z),
+    }
+}
