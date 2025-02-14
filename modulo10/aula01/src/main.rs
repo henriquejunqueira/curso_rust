57e74111638077b52afc539e103ad638dd6759e5
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-02-13 23:57:34 -0300

    Trabalhando com Vec

---
+++ modulo10/aula01/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {
+    // cria uma lista. Em [u8; 5] é definido [tipo; tamanho]
+    let lista: [u8; 5] = [1, 2, 3, 4, 5];
+
+    println!("Valor na posição 2: {}", lista[2]);
+
+    let mut numeros: Vec<u8> = Vec::new();
+
+    numeros.push(1);
+    numeros.push(2);
+    numeros.push(3);
+    numeros.push(4);
+    numeros.push(5);
+
+    println!("Valores do vetor: {:?}", numeros);
+
+    for n in numeros {
+        println!("{}", n);
+    }
+}
